e10a5b973d635ec57b4043e268e3d17390899b27
mintmacaroon <rjsah3244@naver.com>
2024-05-03 19:46:04 +0900

    Initial commit

---
+++ Assets/CookAppsSurvival/Scripts/Opening/OpeningSceneManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OpeningSceneManager : MonoBehaviour
+{
+    [SerializeField] Button button_InGame;
+    [SerializeField] Button button_Setting;
+
+    private void Awake()
+    {
+        button_InGame.onClick.AddListener(OnClick_InGame);
+        button_Setting.onClick.AddListener(OnClick_Setting);
+    }
+
+    private void OnClick_InGame()
+    {
+        //SceneLoadingManager.LoadScene("Lobby", true);
+        SceneManager.LoadScene("Lobby");
+    }
+    
+    private void OnClick_Setting()
+    {
+
+    }
+}
